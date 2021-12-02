17925d609574041445fb400e99074c11de7f13db
MR.Kim <kimskingking@gmail.com>
2021-12-02 14:25:57 +0900

    add java

--- app/src/main/java/com/example/d2db/MainActivity.java
+++ app/src/main/java/com/example/d2db/MainActivity.java
@@ -2,14 +2,35 @@ package com.example.d2db;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    //eldkgjwjql@gmail.com
+
+    EditText searchbar = (EditText) findViewById(R.id.searchbar);
+    Button btnSearch = (Button) findViewById(R.id.btnSearch);
+    Button btnHome = (Button) findViewById(R.id.btnSearchHome);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
+
+        //카테고리보기 눌렀을때
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
