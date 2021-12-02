f232d04ce47507dc084f57469c53d257cadac7bd
MR.Kim <kimskingking@gmail.com>
2021-12-02 15:14:14 +0900

    add java

---
+++ app/src/main/java/com/example/d2db/MainPage_Act.java
@@ -0,0 +1,18 @@
+package com.example.d2db;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainPage_Act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
