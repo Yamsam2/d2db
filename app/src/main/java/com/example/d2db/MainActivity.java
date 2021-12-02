05f48b26d104dc23d5a9e44cda6f79fa6a4aac96
Yamsam2 <eldkgjwjql@gmail.com>
2021-12-02 13:18:57 +0900

    first share

---
+++ app/src/main/java/com/example/d2db/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.d2db;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //eldkgjwjql@gmail.com
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage);
+    }
+}
