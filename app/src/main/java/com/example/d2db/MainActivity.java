6d7aae8b617762f203c1675b0e7a6b673c21eb10
MR.Kim <kimskingking@gmail.com>
2021-12-02 15:18:31 +0900

    add java

--- app/src/main/java/com/example/d2db/MainActivity.java
+++ app/src/main/java/com/example/d2db/MainActivity.java
@@ -14,23 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainpage);
-
-        EditText searchbar = (EditText) findViewById(R.id.searchbar);
-        Button btnSearch = (Button) findViewById(R.id.btnSearch);
-        Button btnHome = (Button) findViewById(R.id.btnSearchHome);
-
-        //카테고리보기 눌렀을때
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
+        setContentView(R.layout.activity_main);
 
 
     }
--- app/src/main/java/com/example/d2db/MainPage_Act.java
+++ app/src/main/java/com/example/d2db/MainPage_Act.java
@@ -1,18 +1,34 @@
 package com.example.d2db;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPage_Act extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.mainpage);
+
+            EditText searchbar = (EditText) findViewById(R.id.searchbar);
+            Button btnSearch = (Button) findViewById(R.id.btnSearch);
+            Button btnHome = (Button) findViewById(R.id.btnSearchHome);
+
+            //카테고리보기 눌렀을때
+            btnHome.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), com.example.d2db.MainActivity.class);
+                    startActivity(intent);
+                }
+            });
 
 
-
+        }
     }
-}
