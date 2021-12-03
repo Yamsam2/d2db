371238adaf96a79bce3c7bbca297a6e4a762bd00
Yamsam2 <eldkgjwjql@gmail.com>
2021-12-03 15:31:18 +0900

    share4

---
+++ app/src/main/java/com/example/d2db/BookmarkPage.java
@@ -0,0 +1,14 @@
+package com.example.d2db;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookmarkPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookmark_page);
+    }
+}
