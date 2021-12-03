50c6fe29bb2cfcb3d7cdce16242e67759ad08d38
Yamsam2 <eldkgjwjql@gmail.com>
2021-12-03 14:20:42 +0900

    share3

--- app/src/main/java/com/example/d2db/MainPage_Act.java
+++ app/src/main/java/com/example/d2db/MainPage_Act.java
@@ -2,17 +2,22 @@ package com.example.d2db;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPage_Act extends AppCompatActivity {
+    private VideoView videov;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,20 @@ public class MainPage_Act extends AppCompatActivity {
 
             mPlayer = MediaPlayer.create(this,R.raw.dia2);
             Switch bgm = (Switch) findViewById(R.id.bgm);
+
+
+            //백그라운드 비디오
+            videov = (VideoView) findViewById(R.id.video_view);
+            Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.backvideoss);
+            videov.setVideoURI(uri);
+            videov.start();
+            //무한루프
+            videov.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.setLooping(true);
+                }
+            });
 
 
 
@@ -67,4 +86,30 @@ public class MainPage_Act extends AppCompatActivity {
 
 
         }
+
+//배경 비디오(videov) 나가거나 다른창에서 돌아왔을 때 다시시작
+    @Override
+    protected void onPostResume() {
+        videov.resume();
+        super.onPostResume();
     }
+
+    @Override
+    protected void onRestart() {
+        videov.start();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        videov.suspend();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        videov.stopPlayback();
+        super.onDestroy();
+    }
+
+}
