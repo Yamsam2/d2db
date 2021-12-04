6ff8da8e5a86aea508af70f92d9e8767cc725ac8
kimskingking <kimskingking@gmail.com>
2021-12-04 15:41:50 +0900

    button dir updated.

--- app/src/main/java/com/example/d2db/ItemPage.java
+++ app/src/main/java/com/example/d2db/ItemPage.java
@@ -1,6 +1,9 @@
 package com.example.d2db;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -16,8 +19,27 @@ public class ItemPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_page);
 
+        Button btnSearchHome = (Button) findViewById(R.id.btnSearchHome);
+        Button btn_bookmark_list = (Button) findViewById(R.id.btn_bookmark_list);
 
-        //상세보기 아이탬 가져오기
+        //카테고리버튼
+        btnSearchHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        //즐겨찾기버튼
+        btn_bookmark_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),BookmarkPage.class);
+                startActivity(intent);
+            }
+        });
+
+        //상세보기 아이템 가져오기
         listView = findViewById(R.id.item_listview_detail);
         adapter = new ListItemAdapter();
         adapter.addItem(new ListItem
